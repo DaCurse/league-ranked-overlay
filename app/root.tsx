769a6dda853806a1f69ea9034b0874350219df20
DaCurse <dacurse0@gmail.com>
2022-03-28 21:39:38 +0300

    Initial commit

---
+++ app/root.tsx
@@ -0,0 +1,37 @@
+import type { LinksFunction, MetaFunction } from "remix";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "remix";
+import tailwindStylesheetUrl from "./styles/tailwind.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
+};
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "Rank Overlay",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export default function App() {
+  return (
+    <html lang="en" className="h-full">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body className="h-full">
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
---
+++ cypress/e2e/smoke.ts
@@ -0,0 +1,5 @@
+describe("smoke tests", () => {
+  it("should visit", () => {
+    cy.visit("/");
+  });
+});
