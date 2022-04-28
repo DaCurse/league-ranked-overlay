afba6787bd98bf9d8aaaf8aa5acadba851e4dcec
DaCurse <dacurse0@gmail.com>
2022-04-29 01:22:59 +0300

    Fix queue type for ranked flex

--- app/riot-api/types/queue.ts
+++ app/riot-api/types/queue.ts
@@ -1,4 +1,4 @@
-export type QueueTypeId = 'RANKED_SOLO_5x5' | 'RANKED_TEAM_5x5'
+export type QueueTypeId = 'RANKED_SOLO_5x5' | 'RANKED_FLEX_SR'
 
 interface QueueType {
   id: QueueTypeId
@@ -7,5 +7,5 @@ interface QueueType {
 
 export const QUEUE_TYPES: ReadonlyArray<QueueType> = [
   { id: 'RANKED_SOLO_5x5', name: 'Ranked Solo/Duo' },
-  { id: 'RANKED_TEAM_5x5', name: 'Ranked Flex' },
+  { id: 'RANKED_FLEX_SR', name: 'Ranked Flex' },
 ] as const
